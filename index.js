6cea0b56c2546cb1fcf8486830f28257eb061f2e
bestest-hue <57276966+bestest-hue@users.noreply.github.com>
2020-11-23 21:30:32 +0000

    Update index.js
    removed !h from activity

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("!h", {type: "STREAMING"});
+    
 
 })
 
